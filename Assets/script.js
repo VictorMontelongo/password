b06649fd932e3cf0310727c81e7d14a7bd1d46ca
Victor Montelongo <victoramontelongo@gmail.com>
2023-10-02 17:25:37 -0500

    pushing current work to save current model

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//Will I need this?
+var finalPassword = ""
+
 // could not add special character " and ] as they seem to destroy the array
-var SpecialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~",];
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~",];
 
-var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var Letters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var lowerCaseLetters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-var numberofCharacters;
-var includeNumbers;
-var includeSpecialCharacters;
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 
-var finalPassword = "";
+var finalArr = []
 
-// Job to generate a random number 
-function getARandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function getARandomSpecialCharacter() {
-
-  return SpecialCharacters.substr(Math.floor(s.length * Math.random()), 1);
-}
-
-function getARandomLetter() {
-  for (var i = 0; i < 0; i++) {
-    result += Letters.charAt(Math.floor(Math.random() * Letters.length));
+// I need to ask the user a list of questions to generate their password
+function getPrompts() {
+  finalArr = [];
+  numberOfCharaters = prompt("How many characters do you want in your password?")
+  //  // I Googled how to confirm and concat this option.
+  if (confirm("Do you need uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseLettersrCaseLetters);
   }
+  if (confirm("Do you need lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseLetters);
+  }
+  if (confirm("Do you need special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharacters);
+  }
+  if (confirm("Do you need numbers in your password?")) {
+    choiceArr = choiceArr.concat(numbers);
+  }
+  return true;
 }
 
 function generatePassword() {
-
-  includeNumbers = confirm("Do you want numbers?")
-
-  var randomNumber = getARandomNumber()
-
-  finalPassword = finalPassword + randomNumber;
-
-  //prompt
-  var name = prompt("What is your name");
-  //confirm
-
-  return ""
+  // this should generate a new password based on the prompts asked above
+  var password = "";
+  for (var i = 0; i < characterLength; i++)
+    var randomIndex = Math.floor(Math.random() * finalArr.length)
+  password = password + finalArr[randomIndex];
+  return password;
 }
 
 // No Touch Write password to the #password input
@@ -55,3 +55,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
