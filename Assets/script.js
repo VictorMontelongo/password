1aa9f96d53e8f39b3446b2a2d87836123df728d0
Victor Montelongo <victoramontelongo@gmail.com>
2023-10-02 18:03:07 -0500

    Third push, saving work as most works, need it to recognize all characters

--- Assets/script.js
+++ Assets/script.js
@@ -2,55 +2,57 @@
 var generateBtn = document.querySelector("#generate");
 
 //Will I need this?
-var finalPassword = ""
+var finalPassword = "";
 
 // could not add special character " and ] as they seem to destroy the array
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+
+var lowerCaseLetters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~",];
 
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var lowerCaseLetters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var numOfCharaters = "";
 
-var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-
-var finalArr = []
+var finalArr = [];
 
 // I need to ask the user a list of questions to generate their password
 function getPrompts() {
   finalArr = [];
-  numberOfCharaters = prompt("How many characters do you want in your password?")
+  numOfCharaters = prompt("How many characters do you want in your password?")
   //  // I Googled how to confirm and concat this option.
   if (confirm("Do you need uppercase letters in your password?")) {
-    choiceArr = choiceArr.concat(upperCaseLettersrCaseLetters);
+    finalArr = finalArr.concat(upperCaseLetters);
   }
   if (confirm("Do you need lowercase letters in your password?")) {
-    choiceArr = choiceArr.concat(lowerCaseLetters);
+    finalArr = finalArr.concat(lowerCaseLetters);
   }
   if (confirm("Do you need special characters in your password?")) {
-    choiceArr = choiceArr.concat(specialCharacters);
+    finalArr = finalArr.concat(specialCharacters);
   }
   if (confirm("Do you need numbers in your password?")) {
-    choiceArr = choiceArr.concat(numbers);
+    finalArr = finalArr.concat(numbers);
   }
   return true;
 }
 
 function generatePassword() {
-  // this should generate a new password based on the prompts asked above
-  var password = "";
-  for (var i = 0; i < characterLength; i++)
+  // this should generate a new password based on the prompts asked above with the for loop
+  var finalPassword = "";
+  for (var i = 0; i < numOfCharaters; i++)
     var randomIndex = Math.floor(Math.random() * finalArr.length)
-  password = password + finalArr[randomIndex];
-  return password;
+  finalPassword = password + finalArr[randomIndex];
+  return finalPassword;
 }
 
 // No Touch Write password to the #password input
 function writePassword() {
+  getPrompts()
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
